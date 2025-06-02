2c568fbf241ce5494f4b4413ea1a671c48e667e2
Abdulhamid <hayyuu09@outlook.com>
2025-06-02 16:43:13 +0300

    ilk yükleme

--- obj/Debug/net9.0/Alhadis.AssemblyInfo.cs
+++ obj/Debug/net9.0/Alhadis.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alhadis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6559a2c68f4a0f321e4f5742eab3fcd3c8507c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243dc61252ed31a04dd6fb47de3d98ce257d6fc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alhadis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alhadis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
